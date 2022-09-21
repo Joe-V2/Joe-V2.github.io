3db406a092e0c50a3ec4c06d446ef213ce2f799a
Joe <joegreaney98@yahoo.com>
2022-09-21 16:10:57 +0100

    added main script

---
+++ main.js
@@ -0,0 +1,26 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementBot = reveals[i].getBoundingClientRect().bottom;
+      var elementVisible = 50;
+  
+      if (elementTop < windowHeight - elementVisible && elementBot > elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  var str = navigator.userAgent;
+  var i = str.indexOf("Instagram");
+  if (i != -1) {    
+    document.write("<a target=\"_blank\" href=\"http://instagram.com/?nibrowser=no\">Proceed to Safari</a>");
+    window.stop();
+  }  
+  window.addEventListener("scroll", reveal);
+  window.addEventListener("DOMContentLoaded", reveal)
+  
