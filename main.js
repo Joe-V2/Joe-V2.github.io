687ec30a8b46516f86b012031c63a603cdc318a1
Joe <joegreaney98@yahoo.com>
2022-09-21 16:25:31 +0100

    more webkit stuff

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function reveal() {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementBot = reveals[i].getBoundingClientRect().bottom;
-      var elementVisible = 50;
+      var elementVisible = 5;
   
       if (elementTop < windowHeight - elementVisible && elementBot > elementVisible) {
         reveals[i].classList.add("active");
@@ -18,7 +18,7 @@ function reveal() {
   var str = navigator.userAgent;
   var i = str.indexOf("Instagram");
   if (i != -1) {    
-    document.write("<a target=\"_blank\" href=\"http://instagram.com/?nibrowser=no\">Proceed to Safari</a>");
+    document.write("<a target=\"_blank \" href=\"http://instagram.com/?nibrowser=no\">Proceed to Safari</a>");
     window.stop();
   }  
   window.addEventListener("scroll", reveal);
